84e0e745e75cde45f0fca44fa9adf8114a9df976
Chordey213 <cpwalla026@gmail.com>
2018-10-22 22:10:09 -0400

    added key ID, updated player , removed unneeded space at bottom

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const players = [
     {
     name:"Cory",
@@ -77,74 +76,3 @@ ReactDOM.render(
     document.getElementById('root')
     
 );
-=======
-const players = [
-    {name:"Cory",
-     score:"50"
-    },
-
-    {name:"Lauren",
-     score:"90"
-    },
-
-    {name:"Arianna",
-     score:"85"
-    },
-
-    {name:"Teddy",
-     score:"80"
-    },
-
-    {name:"Tinny",
-     score:"50"
-    },
-];
-
-const Header = (props) => 
-        <header>
-            <h1>{ props.title }</h1>
-            <span className="stats">{ props.totalPlayers }</span>
-        </header>;
- // component built in Arrow function syntax. No curly braces //
-
-const Player = (props) => 
-
-<div className="player">
-    <span className="player-name">
-        { props.name }
-    </span>
-    <Counter score={ props.score }/>
-</div>
-
-const Counter = (props) =>
-<div className="counter">
-    <button className="counter-action decrement"> - </button>
-    <span className="counter-score">{props.score}</span>
-    <button className="counter-action increment"> + </button>
-</div>
-
-// JSX Comment  = {/* Comment Title */} //
-const App = (props) =>
-    <div className="scoreboard">
-        <Header 
-        title="Scoreboard" 
-        totalPlayers={1}
-        />
-
-        {/* Players List */} 
-        {props.initialPlayers.map(
-            
-        )}
-        <Player name="Cory" score={50}/>
-        <Player name="Lauren" score={90}/>
-        <Player name="Arianna" score={50}/>
-        <Player name="Teddy" score={85}/>
-        <Player name="Tinny" score={80}/>
-    </div>
-
-ReactDOM.render(
-    <App initialPlayers={players}/>,
-    document.getElementById('root')
-    
-);
->>>>>>> c86986bfd6d78ff3bece58e204610623161d9941
