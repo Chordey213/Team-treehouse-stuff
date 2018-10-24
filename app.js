1e3669e458d7ba83d793262572a958c65c5af92e
Chordey213 <cpwalla026@gmail.com>
2018-10-24 19:22:25 -0400

    updated the counter to a component

--- app.js
+++ app.js
@@ -43,15 +43,37 @@ const Player = (props) =>
     <span className="player-name">
         { props.name }
     </span>
-    <Counter score={ props.score }/>
+    <Counter />
 </div>
 
-const Counter = (props) =>
-<div className="counter">
-    <button className="counter-action decrement"> - </button>
-    <span className="counter-score">{props.score}</span>
-    <button className="counter-action increment"> + </button>
-</div>
+class Counter extends React.Component {
+
+    state = {
+        score: 0
+    };
+
+    incrementScore = ()  => {
+        this.setState( prevState => ({
+            score: prevState.score + 1
+            })); 
+    }
+
+    decrementScore = ()  => {
+        this.setState( prevState => ({
+            score: prevState.score - 1
+        }));
+    }
+
+    render() {
+        return (
+        <div className="counter">
+            <button className="counter-action decrement"onClick={this.decrementScore}> - </button>
+            <span className="counter-score">{this.state.score}</span>
+            <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+        </div>
+        );
+    }
+}
 
 // JSX Comment  = {/* Comment Title */} //
 const App = (props) =>
@@ -65,7 +87,6 @@ const App = (props) =>
         {props.initialPlayers.map( player =>
         <Player 
             name={player.name} 
-            score={player.score}
             key={player.id.toString()}
         />
         )}
